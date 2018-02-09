afdd8c9fcfa7aa5a3179a175cdcbbf34c75bc061
Ian Jennings <ian@meetjennings.com>
2018-02-08 22:26:28 -0600

    update before js

--- javascript/facebook-before.js
+++ javascript/facebook-before.js
@@ -1,33 +1,31 @@
 export default (request, response) => {
 
-    console.log('run')
+    console.log('run 1')
 
     const db = require('kvstore');
 
     let headersObject = request.headers;
 
-    let method = request.method;
-    let params = request.params;
-    let body = request.body;
+    console.log('here 5')
 
-    let authKey = (method == 'get') ? params.authKey : body.authKey;
-    let uuid = (method == 'get') ? params.uuid : body.uuid;
+    console.log(request);
 
-    console.log('run before publish')
+    // let key = ['valid', uuid].join(':');
 
-    // console.log('run', authKey, uuid);
-    // return request.ok();
+    // console.log(key)
 
-    return db.get(['valid', uuid]).then((dbKey) => {
+    // return db.get(key).then((dbKey) => {
 
-        if(authKey === dbKey) {
-            response.status = 200;
-        }  else {
-            response.status = 401;
-        }
+    //     console.log('db get response')
 
-        return response.send();
+    //     if(authKey === dbKey) {
+    //         return request.ok();
+    //     }  else {
+    //         return request.abort();
+    //     }
 
-    });
+    // }).catch((err) => {
+    //     return request.abort();
+    // });
 
 };
